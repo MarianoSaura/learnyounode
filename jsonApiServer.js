590f62aa81a9219dbe3d9c2a579e6180817358b3
Mariano Saura <1784443@alu.murciaeduca.com>
2016-10-14 12:59:09 +0200

    último ejercicio terminado

--- jsonApiServer.js
+++ jsonApiServer.js
@@ -1,6 +1,59 @@
 var http = require('http');
 var url = require('url');
 
+var server = http.createServer(function(req, res) {
+
+	var objURL = url.parse(req.url, true);
+	var fecha = new Date(objURL.query.iso);
+	var objJSON;
+
+	//res.write(JSON.stringify(objURL))
+	if(req.method === 'GET'){
+		if (objURL.pathname === '/api/parsetime') {
+
+			objJSON = {
+
+				'hour': fecha.getHours(),
+				'minute': fecha.getMinutes(),
+				'second': fecha.getSeconds()
+
+			}
+
+			res.end(JSON.stringify(objJSON));
+			
+
+		}else if(objURL.pathname === '/api/unixtime'){
+
+			objJSON = {
+
+				'unixtime': fecha.getTime()
+
+			}
+
+
+		}else{
+
+	        res.writeHead(404, { 'Content-Type': 'application/json' }); 
+
+			objJSON= {'error' : 'no has elegido la opción correcta'};
+
+		}
+
+	}else{
+
+		res.writeHead(405, { 'Content-Type': 'application/json' }); 
+	}
+
+	res.end(JSON.stringify(objJSON));
+
+})
+
+server.listen(process.argv[2], function() {
+	console.log("Escuchando en el puerto:", process.argv[2])
+});
+
+/*
+
 var parte = url.parse(request.url, true);
 
 if(parte.pathname == "/api/parsetime"){
@@ -18,3 +71,4 @@ res.writeHead(200, { 'Content-Type': 'application/json' });
 server.listen(process.argv[2], function(){
 	console.log("Escuchando en el puerto:", process.argv[2]);
 });
+*/
